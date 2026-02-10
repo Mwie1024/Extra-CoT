6feeb5f15942688712bbeec99afde3b63ca24eef
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 00:14:24 +0800

    change folder structure

--- baselines/tokenskip/generate_data.sh
+++
@@ -1,5 +0,0 @@
-python longformer_tokenizer_compression.py \
-  --input_path /data/tyt/workspace/tyt/Code/efficient_reasoning/TokenSkip-main/outputs/Qwen2.5-7B-Instruct/gsm8k/7b/Original/train/samples/predictions_formatted.jsonl \
-  --model_dir  /data/tyt/workspace/tyt/CoT/CoT-Language-master/train_longformer/ckpt_longformer_large_full_data \
-  --output_dir ./Compression \
-  --ratios 0.9,0.8,0.7,0.6,0.5,0.4 \
--- baselines/tokenskip/get_llama.sh
+++
@@ -1,5 +0,0 @@
-python get_llamafactory.py \
-  --original /data/tyt/workspace/tyt/Code/efficient_reasoning/TokenSkip-main/outputs/Qwen2.5-7B-Instruct/gsm8k/7b/Original/train/samples/predictions_formatted.jsonl \
-  --comp-dir ./Compression \
-  --ratios 1.0,0.9,0.8,0.7,0.6,0.5 \
-  --out ./outputs/test.json
