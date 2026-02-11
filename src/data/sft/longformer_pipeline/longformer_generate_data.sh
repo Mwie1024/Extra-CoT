f37b36b70ab31621fb5917ee600b393d912b6b1c
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 16:56:15 +0800

    Update input and model directory paths in script

--- src/data/sft/longformer_pipeline/longformer_generate_data.sh
+++ src/data/sft/longformer_pipeline/longformer_generate_data.sh
@@ -1,5 +1,5 @@
 python longformer_compressor.py \
-  --input_path /data/tyt/workspace/tyt/CoT/CoT-Language-master/Qwen3-1.7B/dataset/metamath_145k_query/selected_sft_seed_15k.jsonl \
-  --model_dir  /data/tyt/workspace/tyt/CoT/CoT-Language-master/train_longformer/ckpt_longformer_large_full_data \
+  --input_path input.jsonl \
+  --model_dir  model_path \
   --output_dir ./Compression/SFT_15k \
   --ratios 0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1
