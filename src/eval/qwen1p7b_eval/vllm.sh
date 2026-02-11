22c2c07bc13d58e5c9d30afce9b9fc3b61f3b169
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 17:01:28 +0800

    Update vllm.sh

--- src/eval/qwen1p7b_eval/vllm.sh
+++ src/eval/qwen1p7b_eval/vllm.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=5 python -m vllm.entrypoints.openai.api_server \
-    --model /data/jbh/verl_ckpt/ablation/no_comp/step_200 \
+    --model model_path \
     --tensor-parallel-size 1 \
     --host 0.0.0.0 \
     --port 8003 \
