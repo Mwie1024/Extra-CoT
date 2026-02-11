c78b8e3c24d4a3249e322715fd59a3caa9fe0d5f
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 17:06:20 +0800

    Update vllm_inference.sh

--- src/training/qwen1p7b_rl/dataset/vllm_inference.sh
+++ src/training/qwen1p7b_rl/dataset/vllm_inference.sh
@@ -1,4 +1,6 @@
-python vllm_inference.py --input /data/tyt/workspace/tyt/CoT/CoT-Language-master/Qwen3-1.7B/RL/dataset/metamath_60k.jsonl \
+python vllm_inference.py 
+  --input data_path \
   --output_dir ratio_output_60k_0.8_1.0 \
-  --base_url http://localhost:8001/v1 --model /data/tyt/workspace/tyt/CoT/LLaMA-Factory-main/saves/qwen3_1.7b_full_ratio_72k_cleaned \
+  --base_url http://localhost:8001/v1 \
+  --model model_path \
   --model_type qwen --processes 128 --samples 1 --ratios 0.8,1.0
