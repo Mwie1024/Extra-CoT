5c8945c8c5c05c6848d0df8522aad2aefa278e68
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 17:02:54 +0800

    Refactor special_token.sh to use generic paths

--- src/eval/qwen1p7b_eval/special_token.sh
+++ src/eval/qwen1p7b_eval/special_token.sh
@@ -1,8 +1,7 @@
 python special_token_vllm.py \
-  --input /data/tyt/workspace/tyt/CoT/CoT-Language-master/Qwen3-1.7B/eval/dataset/gsm8k/test.jsonl \
-  --dataset_format ansaug \
-  --out ./eval_result_thinkless/300/gsm8k \
+  --input dataset \
+  --out output_dir \
   --vllm_base_url http://localhost:8003/v1 \
-  --vllm_model /data/jbh/verl_ckpt/DeGRPO/step_300 \
-  --tokenizer_path /data/jbh/verl_ckpt/DeGRPO/step_300 \
+  --vllm_model model_path \
+  --tokenizer_path model_path \
   --ratios 3.0
