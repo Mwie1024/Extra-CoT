27e7585549a02d26310b741adb340ef3af1fe07a
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 16:51:05 +0800

    fix

--- src/data/compressor/dataset_preparation/API/api_gpt_4o.sh
+++ src/data/compressor/dataset_preparation/API/api_gpt_4o.sh
@@ -1,7 +1,7 @@
 # 跑标注（输入既可以是 JSON 也可以是 JSONL）
 python gpt4o_ranges_labeler.py \
-  -i /Users/mwie/User/Data/Code/CoT-Language/CoT_Language/dataset_preparation/API/dataset_split/4k_sampled_data.jsonl \
-  -o /Users/mwie/User/Data/Code/CoT-Language/CoT_Language/dataset_preparation/API/test.jsonl \
+  -i input.jsonl \
+  -o output.jsonl \
   --model gpt-4o \
   --max-workers 32 \
   --min-interval 0.12 \
