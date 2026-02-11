00f730d19c82a65125a342cf11ffc0b2a6c4788d
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 16:54:32 +0800

    Update input and model path in preview script

--- src/data/compressor_longformer/eval/preview.sh
+++ src/data/compressor_longformer/eval/preview.sh
@@ -1,6 +1,6 @@
 python preview_longformer.py \
-  -i ./test.jsonl \
-  -m /data/tyt/workspace/tyt/CoT/CoT-Language-master/train_longformer/ckpt_longformer_large_full_data \
+  -i input.jsonl \
+  -m model_path \
   -o ./preview_test.jsonl \
   --ratios 0.1,0.2,0.3,0.5,0.7,0.9 \
   --limit 7
