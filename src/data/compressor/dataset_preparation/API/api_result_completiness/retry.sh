4efd0f8be19b2e78be4ce6a653f6fa44260642f4
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 16:51:34 +0800

    Update input file and parameters in retry script

--- src/data/compressor/dataset_preparation/API/api_result_completiness/retry.sh
+++ src/data/compressor/dataset_preparation/API/api_result_completiness/retry.sh
@@ -1,24 +1,7 @@
 #!/usr/bin/env bash
 set -e
-export NUWA_BASE_URL="https://api.nuwaapi.com/v1"
-export NUWA_API_KEY="sk-IjumAnpWkZ4pwleMSJesj3DghaPp6l8YeLZ6WkBCj8XjWmgV"
-
-# python retry_empty_chunks.py \
-#   -d /Users/mwie/User/Data/Code/CoT-Language/CoT_Language/dataset_preparation/camel/camel_chunk_only_cot/chunked_result.jsonl\
-#   -m ./merged_empty_ranges.jsonl \
-#   -o ./retry_result.jsonl \
-#   --model gpt-4o \
-#   --max-workers 32 \
-#   --min-interval 0.15 \
-#   --max-tokens 256 \
-#   --temperature 0.0 \
-#   --force-json \
-#   --prompt-detailed \
-#   --min-zpad 3 \
-#   --index-basis one_based_closed
-
 python retry_empty_chunks.py \
-  -d /Users/mwie/User/Data/Code/CoT-Language/CoT_Language/dataset_preparation/camel/camel_chunk_only_cot/chunked_result.jsonl\
+  -d input.jsonl\
   -m ./empty.jsonl \
   -o ./retry_result.jsonl \
   --rounds 10 --temperature 0.3 --temp-step 0.15 \
