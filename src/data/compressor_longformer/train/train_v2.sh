f7cd149aec5512723589633479878422a4c0aa7e
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 16:53:50 +0800

    Update training script with new data and model paths

--- src/data/compressor_longformer/train/train_v2.sh
+++ src/data/compressor_longformer/train/train_v2.sh
@@ -1,6 +1,6 @@
 torchrun --nproc_per_node=8 train_longformer_v2.py \
-  --data-path /data/tyt/workspace/tyt/CoT/CoT-Language-master/train_longformer/LONGFORMER_DATA.pt \
-  --model-name /data/tyt/workspace/tyt/Models/longformer-large-4096 \
+  --data-path dataset.pt \
+  --model-name longformer_path \
   --save-dir ./ckpt_longformer_large_full_data \
   --epochs 40 --lr 2e-5 --batch-size 2 \
   --grad-accum 16 --fp16 --use-global-attn \
