e275533bb14c2fe9fdfa44b4a3b20628c30e99d4
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 16:49:00 +0800

    fix

--- src/data/compressor/dataset_preparation/API/api_gpt_4o.sh
+++ src/data/compressor/dataset_preparation/API/api_gpt_4o.sh
@@ -1,6 +1,3 @@
-export NUWA_BASE_URL="https://api.nuwaapi.com/v1"
-export NUWA_API_KEY="sk-IjumAnpWkZ4pwleMSJesj3DghaPp6l8YeLZ6WkBCj8XjWmgV"
-
 # 跑标注（输入既可以是 JSON 也可以是 JSONL）
 python gpt4o_ranges_labeler.py \
   -i /Users/mwie/User/Data/Code/CoT-Language/CoT_Language/dataset_preparation/API/dataset_split/4k_sampled_data.jsonl \
