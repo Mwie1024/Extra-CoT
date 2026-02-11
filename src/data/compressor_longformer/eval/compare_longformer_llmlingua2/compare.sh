7541a9024de76625dda568dc96198f69bed6a894
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 16:55:03 +0800

    Update model paths in compare.sh script

--- src/data/compressor_longformer/eval/compare_longformer_llmlingua2/compare.sh
+++ src/data/compressor_longformer/eval/compare_longformer_llmlingua2/compare.sh
@@ -1,6 +1,6 @@
 python compare.py \
-  --llmlingua_model /data/tyt/workspace/tyt/Models/llm_lingua_2_xlm-roberta-large \
-  --longformer_model /data/tyt/workspace/tyt/CoT/CoT-Language-master/train_longformer/ckpt_longformer_large_full_data \
+  --llmlingua_model llmlingua_path \
+  --longformer_model longformer_path \
   --ratios 0.2,0.4,0.6,0.8 \
   --target_tokens 2500 \
   --output_dir outputs \
