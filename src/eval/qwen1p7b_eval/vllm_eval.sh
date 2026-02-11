bd535bae9080d2f417b223edeb2ffd7dd018cb2a
Mwie1024 <120359235+Mwie1024@users.noreply.github.com>
2026-02-11 17:02:19 +0800

    Update vllm_eval.sh with dynamic input and output paths

--- src/eval/qwen1p7b_eval/vllm_eval.sh
+++ src/eval/qwen1p7b_eval/vllm_eval.sh
@@ -1,9 +1,9 @@
 python vllm_eval.py \
-  --input /data/tyt/workspace/tyt/CoT/CoT-Language-master/Qwen3-1.7B/eval/dataset/math-500/test.jsonl \
+  --input dataset \
   --dataset_format ansaug \
-  --out ./eval_result_ablation/no_comp/200/math \
+  --out output_Dir \
   --vllm_base_url http://localhost:8003/v1 \
-  --vllm_model  /data/jbh/verl_ckpt/ablation/no_comp/step_200 \
-  --tokenizer_path  /data/jbh/verl_ckpt/ablation/no_comp/step_200 \
+  --vllm_model  model_path \
+  --tokenizer_path  model_path \
   --max_new_tokens 4096 \
   --ratio 2.0
